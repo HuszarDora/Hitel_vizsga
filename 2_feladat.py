3f9137a1bb1a79648bb3e7a6c9e833ee1003e678
kgeri777 <gergo.kocsis@stud.uni-corvinus.hu>
2023-06-17 13:11:53 +0200

    2.feladat ábra és function javítás

--- 2_feladat.py
+++ 2_feladat.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -9,6 +10,7 @@ df_spy['Return'] = df_spy['Adj Close'] / df_spy['Adj Close'].shift(1) - 1
 df_combined = pd.merge(df_voo[['Date', 'Return']], df_spy[['Date', 'Return']], on='Date', suffixes=('_voo', '_spy'))
 df_combined.dropna(inplace=True)
 
+
 def calculate_covariance_matrix(std_x, std_y, corr):
     cov_xy = std_x * std_y * corr
     cov_matrix = np.array([[std_x ** 2, cov_xy], [cov_xy, std_y**2]])
@@ -19,8 +21,8 @@ def calc_asset_returns(mean, cov, size):
     return np.random.multivariate_normal(mean, cov, size)
 
 
-def simulated_returns(expected_return, std_voo, std_spy, correlation, num_of_simulations):
-    cov_matrix = calculate_covariance_matrix(std_voo, std_spy, correlation)
+def simulated_returns(expected_return, volatility, correlation, num_of_simulations):
+    cov_matrix = calculate_covariance_matrix(volatility[0], volatility[1], correlation)
     return calc_asset_returns(expected_return, cov_matrix, num_of_simulations)
 
 
@@ -28,6 +30,7 @@ Voo_std = np.std(df_voo['Return'])
 Voo_mean = np.mean(df_voo['Return'])
 Spy_std = np.std(df_spy['Return'])
 Spy_mean = np.mean(df_spy['Return'])
+volatility = [Voo_std, Spy_std]
 total_volatility = Voo_std + Spy_std
 expected_return = [Voo_mean, Spy_mean]
 weight_voo = Voo_std / total_volatility
@@ -41,15 +44,23 @@ def calc_historical_var(portf_ret, confidence_level):
     var = abs(sorted_returns[var_index])
     return var
 
-
+varok = []
 nsim = 1000
-correlations = [0.2, 0.4, 0.6, 0.8, 1.0]
+correlations = np.arange(-1, 1, 0.2)
 confidence_level = 0.95
 
 for correlation in correlations:
-    price_path = simulated_returns(expected_return, Voo_std, Spy_std, correlation, nsim)
+    price_path = simulated_returns(expected_return, volatility, correlation, nsim)
     price_path = np.exp(price_path) - 1
     weighted_portf_return = weight_voo * price_path[:, 0] + weight_spy * price_path[:, 1]
 
     VAR = calc_historical_var(weighted_portf_return, confidence_level)
+    varok.append(VAR)
     print(f"Correlation: {correlation}, VaR: {VAR}")
+
+plt.plot(varok, correlations)
+plt.xlabel('VAR')
+plt.ylabel('Correlation')
+plt.title('VAR for different correlation assumptions')
+plt.show()
+
